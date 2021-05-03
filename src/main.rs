13842c27ba1ae519dbd3c9a9edb66d28358975ba
Mingzhen Zhuang <superfrankie621@gmail.com>
2021-05-03 14:47:30 +0800

    rays, a simple camera, and background

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
 mod ray;
 mod vec;
 
+use crate::ray::Ray;
 use crate::vec::Vec3;
-use indicatif::ProgressBar;
 use lodepng::RGB;
 use std::path::Path;
 
 // struct Pixel(usize, usize, usize);
 fn main() {
     // Image
-    const WIDTH: usize = 256;
-    const HEIGHT: usize = 256;
+    const ASPECT_RATIO: f32 = 16.0 / 9.0;
+    const WIDTH: usize = 400;
+    const HEIGHT: usize = (WIDTH as f32 / ASPECT_RATIO) as usize;
+
+    // Camera
+    let viewport_height: f32 = 2.0;
+    let viewport_width: f32 = ASPECT_RATIO * viewport_height;
+    // the distance between the projection plane and the projection point
+    let focal_length: f32 = 1.0;
+
+    let origin = Vec3(0.0, 0.0, 0.0);
+    let horizontal = Vec3(viewport_width, 0.0, 0.0);
+    let vertical = Vec3(0.0, viewport_width, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
+
+    // Render
     let mut pixels: Vec<RGB<u8>> = Vec::with_capacity(WIDTH * HEIGHT);
     for j in (0..HEIGHT).rev() {
         for i in 0..WIDTH {
-            let r: f32 = (i as f32) / (WIDTH - 1) as f32;
-            let g: f32 = (j as f32) / (HEIGHT - 1) as f32;
-            let b: f32 = 0.25;
-            let pixel_color = Vec3(r, g, b);
+            let u: f32 = (i as f32) / (WIDTH - 1) as f32;
+            let v: f32 = (j as f32) / (HEIGHT - 1) as f32;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let pixel_color = r.ray_color();
             let pixel = pixel_color.to_rgb();
             pixels.push(pixel);
         }
--- src/ray.rs
+++ src/ray.rs
@@ -14,4 +14,12 @@ impl Ray {
     pub fn at(&self, t: f32) -> Vec3 {
         self.origin + t * self.direction
     }
+
+    /// 根据 y 值将蓝白做了个线性插值的混合, 我们这里把射线做了个单位化, 以保证 y 的取值范围
+    // (-1.0 < y < 1.0) 。因为我们使用 y 轴做渐变, 所以你可以看到这个蓝白渐变也是竖直的。
+    pub fn ray_color(&self) -> Vec3 {
+        let unit_direction = self.direction.to_unit_vector();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+    }
 }
